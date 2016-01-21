0d87fe34f3384003cd173fff13ec03b683e1c004
FoxCarlos Trabajo <foxcarlos@gmail.com>
2016-01-21 16:10:33 -0430

    modificado main.js a namespaces

--- js/main.js
+++ js/main.js
@@ -1,69 +1,79 @@
-var plantilla = function(id){
-    return _.template( $('#' + id).html() );
-};
+(function() {
+    window.App = {
+        Models: {},
+        Collections: {},
+        Views: {}
+    };
 
-var Person = Backbone.Model.extend({
-	defaults:{
-		nombre: 'Usuario Guest',
-		edad: '1',
-		ocupacion: 'Trabajador'
-	}
-});
+    window.plantilla = function(id){
+           return _.template( $('#' + id).html() );
+    };
 
-//Creacion de Coleccion basado en el Modelo de una persona
-var PeopleCollection = Backbone.Collection.extend({
-    model: Person
-});
-
-//Vista para un Modelo de personas
-var PersonView = Backbone.View.extend({
-	tagName: 'li',
-
-	miPlantilla: plantilla('personaPlantilla'),  // _.template($('#personaPlantilla').html()) ,
-
-	initialize: function(){
-		this.render();
-	},
-
-	render: function(){
-		this.$el.html(this.miPlantilla(this.model.toJSON()));
-        return this;
-	}
-});
-
-//Vista para Coleccion de Personas
-var PeopleView = Backbone.View.extend({
-    tagName: 'ul',
-
-    initialize: function(){
-        console.log('metodo initialize de PeopleView');
-    },
-
-    render: function(){
-        this.collection.each(function(person){
-            var personaVista = new PersonView({model: person});
-            this.$el.append(personaVista.render().el);
-        }, this);
-        return this;
-    }
-
-});
-
-//Agregar datos a una coleccion
-var peopleCollection = new PeopleCollection([
-        {
-            nombre: 'Carlos', edad: 43, ocupacion:'Developer'
-        },
-        {
-            nombre:'Nairesther', edad:26, ocupacion:'Educadora'
-        },
-        {
-            nombre: 'Carla', edad:6, ocupacion: 'Estudiante'
-        },
-        {
-            nombre: 'Carla', edad:6, ocupacion: 'Estudiante'
+    // Person Model
+    App.Models.Person = Backbone.Model.extend({
+        defaults:{
+            nombre: 'Usuario Guest',
+            edad: '1',
+            ocupacion: 'Trabajador'
         }
-        ]);
+    });
 
-var peopleView = new PeopleView({ collection: peopleCollection });
-$(document.body).append(peopleView.render().el);   // adding people view in DOM.. Only for demo purpose...
+    //Lista de Personas
+    App.Collections.People = Backbone.Collection.extend({
+        model: App.Models.Person
+    });
+
+
+    //Vista para varias de Personas
+    App.Views.People = Backbone.View.extend({
+        tagName: 'ul',
+
+        initialize: function(){
+            console.log('metodo initialize de PeopleView');
+        },
+
+        render: function(){
+            this.collection.each(function(person){
+                var personaVista = new App.Views.Person({model: person});
+                this.$el.append(personaVista.render().el);
+            }, this);
+
+            return this;
+        }
+
+    });
+
+    //Vista para una persona
+    App.Views.Person = Backbone.View.extend({
+        tagName: 'li',
+
+        miPlantilla: plantilla('personaPlantilla'),  // _.template($('#personaPlantilla').html()) ,
+        /*initialize: function(){
+            this.render();
+        },*/
+
+        render: function(){
+            this.$el.html(this.miPlantilla(this.model.toJSON()));
+            return this;
+        }
+    });
+
+    //Agregar datos a una coleccion
+    var peopleCollection = new App.Collections.People([
+            {
+                nombre: 'Carlos', edad: 43, ocupacion:'Developer'
+            },
+            {
+                nombre:'Nairesther', edad:26, ocupacion:'Educadora'
+            },
+            {
+                nombre: 'Paola', edad:15, ocupacion: 'Estudiante'
+            },
+            {
+                nombre: 'Carla', edad:6, ocupacion: 'Estudiante'
+            }
+            ]);
+
+    var peopleView = new App.Views.People({ collection: peopleCollection });
+    $(document.body).append(peopleView.render().el);   // adding people view in DOM.. Only for demo
+})();
